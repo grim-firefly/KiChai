b4ba32d73099d3993158ca90b471e40eeea51827
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-09-13 03:50:30 +0600

    changed dynamic

---
+++ Admin Panel/Controller/Category/Category.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace kichaiAdmin\Category;
+
+use PDO;
+use PDOException;
+
+class Category
+{
+	private $host = 'localhost';
+	private $port = '8080';
+	private $dbname = 'kichai';
+	private $user = 'root';
+	private $password = '';
+	public  $dbh = null;
+	public function __construct()
+	{
+		try {
+
+			$this->dbh = new PDO("mysql:host=$this->host;port:$this->port;dbname=$this->dbname", $this->user, $this->password);
+		} catch (PDOException $e) {
+			echo 'Error: ' . $e->getMessage();
+		}
+	}
+	public function index()
+	{
+		$query = "SELECT * FROM $this->dbname.category";
+		$ctgquery = $this->dbh->prepare($query);
+		$flag = $ctgquery->execute();
+		if ($flag) {
+			$ctgdata = $ctgquery->fetchAll(PDO::FETCH_ASSOC);
+			return $ctgdata;
+		}
+		return [];
+	}
+
+	public function delete($id)
+	{
+		$query = "DELETE FROM $this->dbname.category WHERE id=?";
+		$ctgquery = $this->dbh->prepare($query);
+		$flag = $ctgquery->execute([$id]);
+		if ($flag) {
+			return true;
+		}
+		return false;
+	}
+	public function update($id, $name)
+	{
+		$query = "UPDATE $this->dbname.category SET category_name=? WHERE id=?";
+		$ctgquery = $this->dbh->prepare($query);
+		$flag = $ctgquery->execute([$name, $id]);
+		if ($flag) {
+			return true;
+		}
+		return false;
+	}
+	public function create($name)
+	{
+		$query = "INSERT INTO $this->dbname.category (category_name) VALUES (?)";
+		$ctgquery = $this->dbh->prepare($query);
+		$flag = $ctgquery->execute([$name]);
+		if ($flag) {
+			return true;
+		}
+		return false;
+	}
+}
---
+++ Admin Panel/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '34aaada94c922d1324577397b151519a16f70683',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '34aaada94c922d1324577397b151519a16f70683',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
